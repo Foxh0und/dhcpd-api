42247573b975bd2eb78a94bf035683b45ff256a8
Adam Miritis <amiritis@wolfmail.co>
2020-09-28 20:40:29 +1000

    START: Lease Controller

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
 const app = express();
 
+
+const PORT = process.env.PORT || 5000 || nconf.get;
 
 app.use( '/lease', require( './routes/lease.js' ) );
 app.listen( 5000, () => console.log( `App Started On ${PORT}`));
--- routes/lease.js
+++ routes/lease.js
@@ -1,8 +1,19 @@
 const express = require( 'express' );
+const dhcpd = require( 'dhcpd-node' );
+const fs = require('fs');
+const { default: parseLease } = require('dhcpd-node/dist/dhcpd-lease-parser');
+
 const router = express.Router();
 
+//TODO: Add test.leases to config.
 router.get( '', (request, response ) => {
-    response.status( 400 ).json( {msg: 'Test'});
+    fs.readFile('/home/amiritis/test.leases', 'utf8', function(err, data) {
+        if (err){
+            throw err;
+        } 
+        const leases = parseLease( data );
+        response.status( 200 ).json(leases);
+    });
 } );
 
 module.exports = router;
