4fddd2946a9f994d5f411c15a92117edfb090ac8
Adam Miritis <amiritis@wolfmail.co>
2020-09-29 21:21:56 +1000

    REFACTOR: Health controller rename to Healthz

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 app.use("/lease", require("./routes/lease.js"));
-app.use("/health", require("./routes/health.js"));
+app.use("/healthz", require("./routes/healthz.js"));
 
 app.listen(PORT, () => console.log(`App Started On ${PORT}`));
