f4040f86d5f5f5b8ba2502ad122dbb9dca9620ee
Adam Miritis <amiritis@wolfmail.co>
2020-09-29 21:07:21 +1000

    ADD: Lease GET Controller

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+require("dotenv").config();
 
-const PORT = process.env.PORT || 5000 || nconf.get;
+const PORT = process.env.PORT || 3000;
 
-app.use( '/lease', require( './routes/lease.js' ) );
-app.listen( 5000, () => console.log( `App Started On ${PORT}`));
+app.use("/lease", require("./routes/lease.js"));
+app.listen(5000, () => console.log(`App Started On ${PORT}`));
--- routes/lease.js
+++ routes/lease.js
@@ -1,19 +1,30 @@
-const express = require( 'express' );
-const dhcpd = require( 'dhcpd-node' );
-const fs = require('fs');
-const { default: parseLease } = require('dhcpd-node/dist/dhcpd-lease-parser');
+const express = require("express");
+const fs = require("fs");
+const { default: parseLease } = require("dhcpd-node/dist/dhcpd-lease-parser");
+require("dotenv").config();
 
 const router = express.Router();
 
-//TODO: Add test.leases to config.
-router.get( '', (request, response ) => {
-    fs.readFile('/home/amiritis/test.leases', 'utf8', function(err, data) {
-        if (err){
-            throw err;
-        } 
-        const leases = parseLease( data );
-        response.status( 200 ).json(leases);
-    });
-} );
+router.get("", (request, response) => {
+  fs.readFile(process.env.LEASE_PATH, "utf8", function (err, data) {
+    if (err) {
+      switch (err.errno) {
+        case -2:
+          console.log(err);
+          response.status(404).json({ msg: err.message });
+          return;
+        default:
+          break;
+      }
+    }
+    const leases = parseLease(data);
+    if (leases.length === 0) {
+      response.status(204);
+      return;
+    }
+
+    response.status(200).json(leases);
+  });
+});
 
 module.exports = router;
